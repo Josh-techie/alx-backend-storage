e71256d7de5e4cd3980b4ffce3ee7d355db4641a
Josh-Techie <youse2002@outlook.fr>
2024-01-24 02:17:27 +0100

    102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 '''Task 15's module.
+Improve 12-log_stats.py by adding the top 10 of the most present IPs in
+the collection nginx of the database logs:
+
+The IPs top must be sorted (like the example below)
 '''
 from pymongo import MongoClient
 
 
 def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
+    '''Function that prints stats about Nginx request logs.
     '''
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
+        count_req = len(list(nginx_collection.find({'method': method})))
+        print('\tmethod {}: {}'.format(method, count_req))
+    status_count_checks = len(list(
         nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
-    print('{} status check'.format(status_checks_count))
+    print('{} status check'.format(status_count_checks))
 
 
-def print_top_ips(server_collection):
-    '''Prints statistics about the top 10 HTTP IPs in a collection.
-    '''
+def print_ips_top(server_collection):
+
     print('IPs:')
     request_logs = server_collection.aggregate(
         [
@@ -38,16 +41,15 @@ def print_top_ips(server_collection):
     )
     for request_log in request_logs:
         ip = request_log['_id']
-        ip_requests_count = request_log['totalRequests']
-        print('\t{}: {}'.format(ip, ip_requests_count))
+        ip_count_requests = request_log['totalRequests']
+        print('\t{}: {}'.format(ip, ip_count_requests))
 
 
 def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
+
     client = MongoClient('mongodb://127.0.0.1:27017')
     print_nginx_request_logs(client.logs.nginx)
-    print_top_ips(client.logs.nginx)
+    print_ips_top(client.logs.nginx)
 
 
 if __name__ == '__main__':
